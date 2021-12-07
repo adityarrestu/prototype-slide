d56c40985333bc694651b011a80f24d83dcc9e98
adityarrestu <adit.restu77@gmail.com>
2021-12-07 12:45:22 +0700

    transisi masih terlihat aneh

---
+++ index.js
@@ -0,0 +1,76 @@
+const slider = document.querySelector(".slider");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+// remove
+let remove = () => {
+    slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+    slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
+  });
+}
+
+// activating slide
+let active = () => {
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+}
+
+// change next slide
+let next = () => {
+  remove();
+  
+  slideNumber++;
+  
+  if(slideNumber > (numberOfSlides - 1)){
+    slideNumber = 0;
+  }
+  
+  active();
+}
+
+//image slider next button
+nextBtn.addEventListener("click", () => {
+  next();
+});
+
+//image slider previous button
+prevBtn.addEventListener("click", () => {
+  remove();
+
+  slideNumber--;
+
+  if(slideNumber < 0){
+    slideNumber = numberOfSlides - 1;
+  }
+
+  active();
+});
+
+//image slider autoplay
+let playSlider;
+
+let repeater = () => {
+  playSlider = setInterval(function(){
+    next();
+  }, 5000);
+}
+
+repeater();
+
+//stop the image slider autoplay on mouseover
+// slider.addEventListener("mouseover", () => {
+//   clearInterval(playSlider);
+// });
+
+//start the image slider autoplay again on mouseout
+// slider.addEventListener("mouseout", () => {
+//   repeater();
+// });
+  
